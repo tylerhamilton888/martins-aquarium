5680c0cb936d97928e9bb1770ff0b05d410926f0
tylerhamilton888 <tyler.hamilton978@gmail.com>
2024-04-18 10:57:31 -0400

    many updates

--- scripts/database.js
+++ scripts/database.js
@@ -4,32 +4,43 @@ const database = {
             id: 1,
             name: "Bruce Wayne",
             species: "Red Lipped Batfish",
-            length: "10 Inches",
+            length: 10,
             locationHarvested: "Gotham City",
-            food: "Justice"
+            food: "Justice",
+            image: "batfish-image.jpg"
 
         },
         {
             id: 2,
-            name: "x",
-            species: "X" ,
-            length: "X" ,
-            locationHarvested: "X" ,
-            food: "X"
+            name: "Kroak",
+            species: "Hairy Frogfish" ,
+            length: 20,
+            locationHarvested: "Shrek's Swamp" ,
+            food: "Everything, but mostly souls",
+            image: "frogfish-image.jpg"
 
         },
-        {id: 2,
-            name: "x",
-            species: "X" ,
-            length: "X" ,
-            locationHarvested: "X" ,
-            food: "X"
+        {id: 3,
+            name: "Linus",
+            species: "Lionfish" ,
+            length: "50" ,
+            locationHarvested: "The bucket in the basement, next to the mop" ,
+            food: "Antelope",
+            image: "lionfish-image.jpg"
+
+        },
+        {
+            id: 4,
+            Species: "Example" ,
+            length: 4,
+            locationHarvested: "Dallas",
+            food: "Food"
         }
 
-    ],
+    ]
     
-
+}
 export const getFish = () => {
     return database.fish.map(fish => ({...fish}))
 }
-}
+
---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+import { getFish } from './database.js'
+
+const allFish = getFish()
+
+for (const fish of allFish) {
+    console.log(fish)
+}
+
+// Import the FishList function from the correct module
+import { FishList } from './fishList.js'
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector('#fish')
+
+
+parentHTMLElement.innerHTML = FishList()
